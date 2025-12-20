95ca871bf36b49c839c2881d52cd6c5362eae554
ShaiGeorge <shaijokuttikkatt@gmail.com>
2025-12-21 01:12:17 +0530

    Add geometric animation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import "./styles/Global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import GeometricAnimation from "./components/GeometricAnimation";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio - Vite Setup Complete!</h1>
-      <p>Dark navy theme is working!</p>
+      <div style={{ paddingTop: "100px" }}>
+        <GeometricAnimation />
+        <h1>My Portfolio</h1>
+        <p>Animated geometry is working!</p>
+      </div>
     </div>
   );
 }
